35366cff0d671a68ca07532718b5ba2d1dbac6c9
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-04-14 17:34:07 -0600

    Made a brush function

--- pixel.js
+++ pixel.js
@@ -1,14 +1,19 @@
 var body = document.getElementsByTagName('body')[0];
-var pixelColor = null;
+var pixelColor = 'black';
+var currentColor =  document.getElementsByClassName('currentColor')[0];
+
 //Paint---------->>>>>>>
 var mainWindow = document.getElementsByClassName('mainWindow')[0];
 mainWindow.addEventListener('click', changeColor);
 
 function changeColor() {
   pixel = event.target;
-  pixel.style.backgroundColor = color;
+  if(pixel.className === 'pixel'){
+    pixel.style.backgroundColor = pixelColor;
+  };
 }
 
+//Changing Brush Colors--------->>>>
 var colors = document.getElementsByClassName('colors')[0];
 colors.addEventListener('click', storeColor);
 
@@ -16,9 +21,36 @@ function storeColor() {
   targets = event.target;
   let colorStyle = window.getComputedStyle(targets);
   pixelColor = colorStyle.backgroundColor;
+  currentColor.style.backgroundColor = pixelColor;
+}
+//The Brush Function-------->>>>
+var down = false;
+
+function isDown() {
+  down = true;
+  console.log(down);
 }
 
+function ifDown(){
+  var target = event.target;
+  if(down && target.className === 'pixel'){
+    target.style.backgroundColor = pixelColor;
+    console.log(pixelColor);
+  }
+}
+
+function isUp() {
+  down = false;
+  console.log(down);
+}
+mainWindow.addEventListener('mousedown', isDown);
+mainWindow.addEventListener('mouseover', ifDown);
+body.addEventListener('mouseup', isUp);
+
+
 //Makes The Board----------->>>>
+
+
 function makePixel(){
   var pixels = document.createElement('div');
   pixels.className = 'pixel';
